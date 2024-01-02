1ca7e18bef706d7ba682f2f634751abb44c3eb81
Rob Whitby <rob.whitby@springer.com>
2024-01-02 18:27:11 +0000

    tweaks

--- src/main/scala/day22.scala
+++ src/main/scala/day22.scala
@@ -8,10 +8,17 @@ object day22 {
     val points = (for {x <- a.x to b.x; y <- a.y to b.y; z <- a.z to b.z} yield Point(x, y, z)).toSet
 
     def downOne() = Brick(a.copy(z = a.z-1), b.copy(z = b.z-1), label)
+
+    def upOne() = Brick(a.copy(z = a.z+1), b.copy(z = b.z+1), label)
+
+    def overlaps(b: Brick) = b.points.exists(points.contains)
   }
 
   case class Stack(bricks: Set[Brick] = Set.empty) {
-    def canMove(from: Brick, to: Brick): Boolean = {
+
+    lazy val sortedBricks = bricks.toSeq.sortBy(_.a.z)
+
+    def canFall(from: Brick, to: Brick): Boolean = {
       if (to.a.z < 1) return false
       val diff = to.points diff from.points
       !bricks.exists(_.points.exists(diff.contains))
@@ -21,19 +28,20 @@ object day22 {
       @tailrec
       def fallBrick(stack: Stack, brick: Brick): Stack = {
         val to = brick.downOne()
-        if (stack.canMove(brick, to)) {
+        if (stack.canFall(brick, to)) {
           fallBrick(Stack(stack.bricks - brick + to), to)
         }
         else stack
       }
 
-      bricks
-        .toSeq
-        .sortBy(_.a.z)
-        .foldLeft(Stack()){ (s,b) => fallBrick(Stack(s.bricks + b), b) }
+      sortedBricks.foldLeft(Stack()){ (s,b) => fallBrick(Stack(s.bricks + b), b) }
     }
 
-    def wouldFall(bs: Set[Brick]): Boolean = bs.exists(b => canMove(b, b.downOne()))
+    def stable(bs: Set[Brick]): Boolean = !bs.exists(b => canFall(b, b.downOne()))
+
+    def bricksAbove(brick: Brick): Set[Brick] = bricks.filter(_.overlaps(brick.upOne()))
+
+    def wouldFall(bs: Set[Brick]): Set[Brick] = bs.filter(b => canFall(b, b.downOne()))
   }
 
   def parseInput(input: List[String]): Stack = {
@@ -49,12 +57,13 @@ object day22 {
     val stack = parseInput(input).fall()
 
     stack.bricks.count{ b =>
-      val above = stack.bricks.filter(_.a.z == b.b.z + 1)
-      !Stack(stack.bricks - b).wouldFall(above)
+      Stack(stack.bricks - b).stable(stack.bricksAbove(b))
     }
   }
 
   def part2(input: List[String]): Long = {
+    val stack = parseInput(input).fall()
+
     ???
   }
 
