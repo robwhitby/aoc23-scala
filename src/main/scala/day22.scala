0fb1c40c66e29a7853483f5acafdc054f43fa46d
Rob Whitby <rob.whitby@springer.com>
2024-01-05 11:50:47 +0000

    22.2

--- src/main/scala/day22.scala
+++ src/main/scala/day22.scala
@@ -1,4 +1,6 @@
 import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.collection.parallel.CollectionConverters.*
 
 object day22 {
 
@@ -7,64 +9,65 @@ object day22 {
   case class Brick(a: Point, b: Point, label: Char) {
     val points = (for {x <- a.x to b.x; y <- a.y to b.y; z <- a.z to b.z} yield Point(x, y, z)).toSet
 
-    def downOne() = Brick(a.copy(z = a.z-1), b.copy(z = b.z-1), label)
+    def down() = Brick(a.copy(z = a.z-1), b.copy(z = b.z-1), label)
 
-    def upOne() = Brick(a.copy(z = a.z+1), b.copy(z = b.z+1), label)
-
-    def overlaps(b: Brick) = b.points.exists(points.contains)
+    def up() = Brick(a.copy(z = a.z+1), b.copy(z = b.z+1), label)
   }
 
-  case class Stack(bricks: Set[Brick] = Set.empty) {
+  case class Stack(bricks: Vector[Brick] = Vector.empty) {
 
-    lazy val sortedBricks = bricks.toSeq.sortBy(_.a.z)
-
-    def canFall(from: Brick, to: Brick): Boolean = {
-      if (to.a.z < 1) return false
-      val diff = to.points diff from.points
-      !bricks.exists(_.points.exists(diff.contains))
-    }
-
-    def fall(): Stack = {
-      @tailrec
-      def fallBrick(stack: Stack, brick: Brick): Stack = {
-        val to = brick.downOne()
-        if (stack.canFall(brick, to)) {
-          fallBrick(Stack(stack.bricks - brick + to), to)
-        }
-        else stack
+    @tailrec
+    private def fallBrick(stack: Stack, brick: Brick): Stack =
+      if (brick.a.z > 1 && stack.bricksBelow(brick).isEmpty) {
+        fallBrick(stack, brick.down())
       }
+      else Stack(stack.bricks.appended(brick))
 
-      sortedBricks.foldLeft(Stack()){ (s,b) => fallBrick(Stack(s.bricks + b), b) }
-    }
+    def fall(): Stack = bricks.foldLeft(Stack())(fallBrick)
 
-    def stable(bs: Set[Brick]): Boolean = !bs.exists(b => canFall(b, b.downOne()))
+    def bricksAbove(brick: Brick): Set[Brick] = overlap(brick, brick.up())
 
-    def bricksAbove(brick: Brick): Set[Brick] = bricks.filter(_.overlaps(brick.upOne()))
+    def bricksBelow(brick: Brick): Set[Brick] = overlap(brick, brick.down())
 
-    def wouldFall(bs: Set[Brick]): Set[Brick] = bs.filter(b => canFall(b, b.downOne()))
+    def overlap(from: Brick, to: Brick): Set[Brick] =
+      val diff = to.points diff from.points
+      bricks.filter(_.points.exists(diff.contains)).toSet
+
+    def wouldFallWithout(bs: Set[Brick]): Set[Brick] = bs
+      .flatMap(bricksAbove)
+      .filter{ bricksBelow(_).forall(bs.contains) }
+
+    def memoize[K, V](fn: K => V): K => V =
+      val cache = mutable.HashMap[K, V]()
+      key => cache.getOrElseUpdate(key, fn(key))
+
+    val wouldFallWithoutCached = memoize(wouldFallWithout)
+
+    @tailrec
+    final def countFallers(without: Set[Brick], count: Long = 0): Long =
+      val wouldFall = wouldFallWithout(without)
+      if (wouldFall.isEmpty) count
+      else countFallers(wouldFall, count + wouldFall.size)
   }
 
-  def parseInput(input: List[String]): Stack = {
+  def parseInput(input: List[String]): Stack =
     val pattern = """(.+),(.+),(.+)~(.+),(.+),(.+)""".r
     val bricks = input.zipWithIndex.map{
       case (pattern(x1,y1,z1,x2,y2,z2), i) =>
         Brick(Point(x1.toInt,y1.toInt,z1.toInt), Point(x2.toInt,y2.toInt,z2.toInt), (i+65).toChar)
     }
-    Stack(bricks.toSet)
-  }
+    Stack(bricks.sortBy(_.a.z).toVector)
 
-  def part1(input: List[String]): Long = {
+  def part1(input: List[String]): Long =
     val stack = parseInput(input).fall()
+    stack
+      .bricks
+      .count(b => stack.wouldFallWithout(Set(b)).isEmpty)
 
-    stack.bricks.count{ b =>
-      Stack(stack.bricks - b).stable(stack.bricksAbove(b))
-    }
-  }
-
-  def part2(input: List[String]): Long = {
+  def part2(input: List[String]): Long =
     val stack = parseInput(input).fall()
-
-    ???
-  }
-
+    stack
+      .bricks
+      .map(b => stack.countFallers(Set(b)))
+      .sum
 }
