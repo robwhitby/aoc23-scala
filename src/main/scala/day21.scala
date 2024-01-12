80fea70fdf7a38e47de5cf6a75a01d60281379b2
Rob Whitby <rob.whitby@springer.com>
2024-01-12 22:02:37 +0000

    finished 21.2

--- src/main/scala/day21.scala
+++ src/main/scala/day21.scala
@@ -19,17 +19,35 @@ case class day21(input: List[String]) {
     val y = if my >= 0 then my else my + height
     input(y)(x) != '#'
 
-  def reachable(steps: Int): Int =
+  def reachable(steps: Long): Long =
     val modSteps = steps % 2
     @tailrec
     def rec(next: Set[Point] = Set(start), reached: Set[Point] = Set.empty, count: Int = 0): Int =
       if (count == steps) reached.size
       else
-        val ps = next.flatMap(_.neighbours())
+        val ps = next.flatMap(_.neighbours()).diff(reached)
         rec(ps, if count % 2 != modSteps then reached ++ ps else reached, count+1)
     rec()
 
   def part1(steps: Int): Long = reachable(steps)
 
-  def part2(steps: Int): Long = reachable(steps)
+  // f(rem + n*2w) = steps
+  // f(rem), f(rem + 2w), f(rem + 4w), ...
+  //
+  // r = f(0) = c
+  // s = f(1) = a + b + c
+  // t = f(2) = 4a + 2b + c
+
+  // c = r
+  // b = s - c - a
+  // a = (t - c)/2 - s + c
+  def part2(steps: Long): Long =
+    val rem = steps % (width*2)
+    val (r, s, t) = (reachable(rem), reachable(rem + width*2), reachable(rem + width*4))
+    val c = r
+    val a = (t - c)/2 - s + c
+    val b = s - c - a
+    def f(x: Long) = a*x*x + b*x + c
+
+    f(steps / (width * 2))
 }
